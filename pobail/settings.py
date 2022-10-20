d53f2e723b3761fb1583be19b89836c4e7191cd0
Gökhan Mete ERTÜRK <92143124+gokhanmeteerturk@users.noreply.github.com>
2022-10-20 14:01:26 +0300

    removed old key

--- pobail/settings.py
+++ pobail/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@876m&g2$*55!90p5cvqfsb)_f07n#33vhp2^3ggabcx#zyjr'
+# SECRET_KEY = "..."
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
