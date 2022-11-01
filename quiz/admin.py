64d12090d4b29a1a6c0d65721662c8bc0c6335ae
Gökhan Mete ERTÜRK <92143124+gokhanmeteerturk@users.noreply.github.com>
2022-11-02 00:04:49 +0300

    Update admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from . models import Quiz, Question, MemberAnswer
-# Register your models here.
+
+
+
+# Register models here.
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(MemberAnswer)
